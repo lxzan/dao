6b6bd85b1df075a33fe8847f3d8a09333b9d4f50
Caster <lxzan@foxmail.com>
2021-12-08 16:10:48 +0800

    impl compare

--- types/compare.go
+++ types/compare.go
@@ -8,10 +8,130 @@ const (
 	Greater Ordering = 1
 )
 
-//type comparable[T any] interface{ type int8, int16, int32, int64, int, uint8, uint16, uint32, uint64, uint, string }
+type comparable[T any] interface {
+	Compare(a, b T) Ordering
+}
 
-//type inteage[T any] interface{ type int, int64, int32, int16, int8, uint, uint64, uint32, uint16, uint8 }
+type (
+	String string
+	Uint   uint
+	Uint64 uint64
+	Uint32 uint32
+	Uint16 uint16
+	Uint8  uint8
+	Int    int
+	Int64  int64
+	Int32  int32
+	Int16  int16
+	Int8   int8
+)
 
-//type interator[Collection any, Iterator any] interface {
-//	ForEach(func(Iterator))
-//}
+func (c String) Compare(a, b String) Ordering {
+	if a > b {
+		return Greater
+	} else if a == b {
+		return Equal
+	} else {
+		return Less
+	}
+}
+
+func (c Uint) Compare(a, b Uint) Ordering {
+	if a > b {
+		return Greater
+	} else if a == b {
+		return Equal
+	} else {
+		return Less
+	}
+}
+
+func (c Uint64) Compare(a, b Uint64) Ordering {
+	if a > b {
+		return Greater
+	} else if a == b {
+		return Equal
+	} else {
+		return Less
+	}
+}
+
+func (c Uint32) Compare(a, b Uint32) Ordering {
+	if a > b {
+		return Greater
+	} else if a == b {
+		return Equal
+	} else {
+		return Less
+	}
+}
+
+func (c Uint16) Compare(a, b Uint16) Ordering {
+	if a > b {
+		return Greater
+	} else if a == b {
+		return Equal
+	} else {
+		return Less
+	}
+}
+
+func (c Uint8) Compare(a, b Uint8) Ordering {
+	if a > b {
+		return Greater
+	} else if a == b {
+		return Equal
+	} else {
+		return Less
+	}
+}
+
+func (c Int) Compare(a, b Int) Ordering {
+	if a > b {
+		return Greater
+	} else if a == b {
+		return Equal
+	} else {
+		return Less
+	}
+}
+
+func (c Int64) Compare(a, b Int64) Ordering {
+	if a > b {
+		return Greater
+	} else if a == b {
+		return Equal
+	} else {
+		return Less
+	}
+}
+
+func (c Int32) Compare(a, b Int32) Ordering {
+	if a > b {
+		return Greater
+	} else if a == b {
+		return Equal
+	} else {
+		return Less
+	}
+}
+
+func (c Int16) Compare(a, b Int16) Ordering {
+	if a > b {
+		return Greater
+	} else if a == b {
+		return Equal
+	} else {
+		return Less
+	}
+}
+
+func (c Int8) Compare(a, b Int8) Ordering {
+	if a > b {
+		return Greater
+	} else if a == b {
+		return Equal
+	} else {
+		return Less
+	}
+}
